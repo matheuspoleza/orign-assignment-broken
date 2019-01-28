f98d037afdecbd14f08869d1da00f947d58b9f7a
Matheus Poleza <matheus.poleza@gmail.com>
2019-01-28 00:59:07 -0200

    feat(VinCheck): creates component style

--- src/views/VinCheck.tsx
+++ src/views/VinCheck.tsx
@@ -4,11 +4,12 @@ import { connect } from "react-redux"
 import { VinInput } from "../components/VinInput"
 import { CarInfoPreview } from "../components/CarInfoPreview"
 import { actions, MapState, MapDispatch } from "../store"
+import "./VinCheck.scss"
 type Props = Pick<RootState, "vin" | "vinCheckResult" | "vinValidationError">
 type Actions = Pick<typeof actions, "setVin" | "checkVin">
 
 const VinCheck: React.SFC<Props & Actions> = props => (
-    <div>
+    <div className="VinCheck">
         <div className="Logo" />
         <h3>Decode Your Vehicle Identification Number</h3>
         <VinInput value={props.vin} onChange={props.setVin} error={props.vinValidationError} />
@@ -25,7 +26,4 @@ const mapDispatch: MapDispatch<Actions> = dispatch => ({
     checkVin: () => dispatch(actions.checkVin())
 })
 
-export const VinCheckView = connect(
-    mapState,
-    mapDispatch
-)(VinCheck)
+export const VinCheckView = connect(mapState, mapDispatch)(VinCheck)
