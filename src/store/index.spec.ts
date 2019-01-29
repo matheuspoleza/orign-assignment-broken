17dc6d9e54510b282de6f5bf0e81062198f797e5
Matheus Poleza <matheus.poleza@gmail.com>
2019-01-28 23:48:13 -0200

    fix(store): fix check vin action and adds reducer tests

--- src/store/index.spec.ts
+++ src/store/index.spec.ts
@@ -1,8 +1,76 @@
-import { reducer, initialState } from "."
+import { reducer, initialState, setVin, checkVin, checkVinSuccess } from "."
 
-describe("counter state", () => {
+describe("Vin state", () => {
     describe("reducer", () => {
-        it("gives same state when unknown action is given", () =>
-            expect(reducer(initialState, { type: "fake" })).toEqual(initialState))
+        describe("when sending check vin success action", () => {
+            it("returns check vin result from payload", () => {
+                const vinApiResult: CarInfo = { make: null, model: null, year: null, trim: null, vechicleType: null }
+                const action = { type: checkVinSuccess, payload: vinApiResult }
+
+                const result = reducer(initialState, action)
+
+                expect(result).toEqual({ ...initialState, vinCheckResult: vinApiResult })
+            })
+        })
+
+        describe("when sending check vin action", () => {
+            it("returns initial state with found validation error", () => {
+                const vin = "123"
+                const state = { ...initialState, vin }
+                const action = { type: checkVin }
+                const vinValidationError = "17 chars expected"
+
+                const result = reducer(state, action)
+
+                expect(result).toEqual({ ...state, vinValidationError })
+            })
+
+            it("returns an array with loading state and cmd type", () => {
+                const vin = "SHHFN23607U002758"
+                const state = { ...initialState, vin }
+                const action = { type: checkVin }
+
+                const result = reducer(state, action)
+
+                // TODO validate better cmd type
+                expect(result).toEqual([{ ...state, vinCheckResult: "Loading" }, expect.any(Object)])
+            })
+        })
+
+        describe("when sending set vin action", () => {
+            it("returns check vin result and filtered vin", () => {
+                const vin = "SHHFN23607U002758"
+                const action = { type: setVin, payload: vin }
+
+                const result = reducer(initialState, action)
+
+                expect(result).toEqual({
+                    vin,
+                    vinCheckResult: initialState.vinCheckResult,
+                    vinValidationError: null
+                })
+            })
+
+            it("returns empty vin and null check vin result", () => {
+                const vin = ""
+                const action = { type: setVin, payload: vin }
+
+                const result = reducer(initialState, action)
+
+                expect(result).toEqual({
+                    vin,
+                    vinCheckResult: null,
+                    vinValidationError: null
+                })
+            })
+        })
+
+        it("returns same state when unknown action is given", () => {
+            const action = { type: "fake" }
+
+            const result = reducer(initialState, action)
+
+            expect(result).toEqual(initialState)
+        })
     })
 })
