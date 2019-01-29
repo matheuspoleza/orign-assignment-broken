eb42c89d45289f210b54987628190d6a514ee173
Matheus Poleza <matheus.poleza@gmail.com>
2019-01-28 22:10:51 -0200

    fix(VinInput): prop value with typo

--- src/components/VinInput.spec.tsx
+++ src/components/VinInput.spec.tsx
@@ -3,9 +3,9 @@ import { shallow } from "enzyme"
 import { VinInput } from "./VinInput"
 
 describe("<VinInput>", () => {
-    it("should exist", () => expect(VinInput).toBeDefined())
+    it("renders vin input component", () => expect(VinInput).toBeDefined())
 
-    it("should render", () => {
+    it("renders given value prop", () => {
         const input = shallow(<VinInput value="Invalid VIN" onChange={_ => null} />).find("input")
         expect(input.props().value).toEqual("Invalid VIN")
     })
